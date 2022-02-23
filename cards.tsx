f4fe7f775230d7b4322f4c6d4c4e6719f705d320
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-23 11:38:23 -0800

    test exporting data

---
+++ cards.tsx
@@ -0,0 +1,17 @@
+// array of data
+let test = [
+    {
+        key_one: 'hello',
+        key_two: 'world'
+    },
+    {
+        key_one: 'goodbye',
+        key_two: 'partner'
+    },
+    {
+        key_one: 'greetings',
+        key_two: 'friend'
+    },
+]
+
+export default test;
