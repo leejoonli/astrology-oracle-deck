5c39a12759389488f41a623f2358d976b235c602
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-23 13:05:30 -0800

    add seed data

--- cards.tsx
+++ cards.tsx
@@ -1,17 +1,40 @@
 // array of data
-let test = [
+let cards = [
     {
-        key_one: 'hello',
-        key_two: 'world'
+        name: 'New Moon',
+        tag: 'A new start is coming',
+        meaning: 'The New Moon marks the start of the Waxing cycle and the mid-point of the Dark Moon.  It\'s a dark and veiled time, when the Moon is invisible, and a time of rebirth.  It\'s a time that witches do their work, making wishes and laying down intentions for the new cycle; an intensely magical time, when it\'s easier to pierce the veil to other worlds.'
     },
     {
-        key_one: 'goodbye',
-        key_two: 'partner'
+        name: 'Waxing Crescent Moon',
+        tag: 'Have faith in your dreams',
+        meaning: 'The Waxing Crescent Moon is the second Moon phase in the eight main Moon phases, but even if it\'s not the time of the Waxing Crescent Moon when you pull this card, it still suggests you need to really pursue your dreams.  It\'s the time to put your foot down hard as you chase your goals.'
     },
     {
-        key_one: 'greetings',
-        key_two: 'friend'
+        name: 'First Quarter Moon',
+        tag: 'Your commitment is being tested',
+        meaning: 'In the lunar cycle, the First Quarter Moon comes between the New Mooon and the Full Moon.  It\'s a time when the Sun and Moon are at a hard astrological angle to each other and this can prompt a small crisis.  No matter when you pull this card, you need to see any dramas as a stepping stone to where you want to be.  It can also be a time when you need to stay strong through a storm.'
+    },
+    {
+        name: 'Gibbous Moon',
+        tag: 'You\'re very close to achieving your goal',
+        meaning: 'The Gibbous Moon comes at the very end of the lunar cycle, just before the Full Moon.  She looks like she\'s bulging because she\'s almost a Full Moon, so she\'s nearly fully rounded.  It\'s the culmination of the Waxing cycle and, as such, tends to be a rather intense period of the month.  No matter when you pull this card, it suggests a very ripe time and a very ripe situation.'
+    },
+    {
+        name: 'Full Moon',
+        tag: 'Surrender to the Divine',
+        meaning: 'The Full Moon marks the climax of the lunar cycle, making this card something of a power card.  The Full Moon is often the time when answers are given to questions asked during the New Moon, and pulling this card at any time in the lunar cycle suggests answers will be coming your way before too long.'
+    },
+    {
+        name: 'Disseminating Moon',
+        tag: 'Take time to breathe out',
+        meaning: 'The Disseminating Moon is the first Moon phase after the explosion of energy that comes with the Full Moon.  No matter when you pick this card, it suggests you\'re at a more tranquil point in the life cycle of whatever situation you\'re asking about.  This is not the ideal time to start something new.  The Disseminating Moon is the time to breathe out.  Give yourself some time off and think about what has passed.'
+    },
+    {
+        name: 'Third Quarter Moon',
+        tag: 'Adjustments are required',
+        meaning: 'At the time of the Third Quarter Moon, we know where we have been - but where are we going?  The Moon is now a Half Moon and slipping away from us as she catches less and less light, moving towards her full disappearance at New Moon.  It\'s a time of falling away and a time to reevaluate.  No matter when you draw this card, the teaching is to release and to trust.'
     },
 ]
 
-export default test;
+export default cards;
