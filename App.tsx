d28059662eaeebd8dd136d002a7b4f8dd59a9092
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-24 11:57:09 -0800

    test random number generator

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-// import cards which will be an array of objects i think
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 import cards from './cards'
 
 export default function App() {
@@ -10,13 +9,23 @@ export default function App() {
 
   // function to randomly choose cards which will be stored in state variable
   // if card was already chosen, choose another card
+  const [state, setState] = useState<number>();
+
+  function randomIntFromInterval() { // min and max included 
+    setState(Math.floor(Math.random() * (44 - 1 + 1) + 1));
+  }
+
+  useEffect(() => {
+
+  }, [state])
 
   return (
     // pressable for card choosing which will call card choosing function
     <View style={styles.container}>
       {/* display cards once state variable changes */}
-      <Text>hello world</Text>
+      <Text>{state}</Text>
       <StatusBar style="auto" />
+      <Pressable onPress={randomIntFromInterval}><Text>Press me</Text></Pressable>
     </View>
   );
 }
