a4224bea818a0b543735614583114ea124c3b9ae
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-24 13:16:18 -0800

    test shuffle

--- App.tsx
+++ App.tsx
@@ -5,18 +5,32 @@ import cards from './cards'
 
 export default function App() {
   // state variable to store selected cards
-  // console.log(cards.length);
+  // console.log(cards[0]);
 
   // function to randomly choose cards which will be stored in state variable
   // if card was already chosen, choose another card
-  const [state, setState] = useState<number>();
 
-  function randomIntFromInterval() { // min and max included 
+  const [state, setState] = useState<number | undefined>();
+  const [test, setTest] = useState<Set<number>>();
+
+  // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+  function randomIntFromInterval() {
     setState(Math.floor(Math.random() * (44 - 1 + 1) + 1));
   }
 
-  useEffect(() => {
+  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  function shuffleArray(array: Array<Object>) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
 
+  useEffect(() => {
+    shuffleArray(cards);
+    // console.log(cards[0]);
   }, [state])
 
   return (
